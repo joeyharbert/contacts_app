62dd56e74745e273cb6ca1eb6fde3279d78ba399
joeyharbert <joey.harbert@gmail.com>
2019-03-04 19:13:18 -0600

    Changes contact_list to restful index (as well as dynamic) and creates show route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,11 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 end
