0150497464821c99373488b14d290ce9536afad4
joeyharbert <joey.harbert@gmail.com>
2019-03-07 19:31:19 -0600

    Makes sure email is in @something.com format

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,13 @@ class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true
+  validate :email_in_proper_format?
+  def email_in_proper_format?
+    if !(email.include?("@") && email[-4..-1] == ".com")
+      errors.add(:email, "not in proper format")
+    end
+  end
+
   def friendly_updated_at
     return updated_at.strftime("%m-%e-%y %H:%M")
   end
