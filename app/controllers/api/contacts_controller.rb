4a0c6c0eea826b7a611782dde08b22f42dc469d7
joeyharbert <joey.harbert@gmail.com>
2019-03-13 19:31:12 -0500

    Adds functionality so that index only shows logged in user's contacts

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,7 +1,11 @@
 class Api::ContactsController < ApplicationController
   def index
-    @contacts = Contact.all
-    render 'index.json.jbuilder'
+    if current_user
+      @contacts = Contact.where(user_id: current_user.id)
+      render 'index.json.jbuilder'
+    else
+      render json: []
+    end
   end
 
   def show
